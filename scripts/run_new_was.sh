5f6e36133382f2faa0ed511ddec667db57024ec2
JJONSOO <pjs970616@naver.com>
2023-06-27 15:07:23 +0900

    nginx 무중단 배포 Test

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -4,12 +4,13 @@
 
 PROJECT_ROOT="/home/ubuntu/app/backend" # 프로젝트 루트
 JAR_FILE="$PROJECT_ROOT/build/libs/spring-webapp.jar" # JAR_FILE (어쩌구저쩌구.jar)
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 # service_url.inc 에서 현재 서비스를 하고 있는 WAS의 포트 번호 가져오기
 CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
-echo "> Current port of running WAS is ${CURRENT_PORT}."
+echo "> Current port of running WAS is ${CURRENT_PORT}." >> &DEPLOY_LOG
 
 if [ ${CURRENT_PORT} -eq 8081 ]; then
   TARGET_PORT=8082 # 현재포트가 8081이면 8082로 배포
@@ -24,11 +25,11 @@ TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-
 
 # PID를 이용해 해당 포트 서버 Kill
 if [ ! -z ${TARGET_PID} ]; then
-  echo "> Kill ${TARGET_PORT}."
+  echo "> Kill ${TARGET_PORT}." >> &DEPLOY_LOG
   sudo kill ${TARGET_PID}
 fi
 
 # 타켓 포트에 jar파일을 이용해 새로운 서버 실행
 nohup java -jar -Dserver.port=${TARGET_PORT} ${JAR_FILE} > /home/ubuntu/nohup.out 2>&1 &
-echo "> Now new WAS runs at ${TARGET_PORT}."
+echo "> Now new WAS runs at ${TARGET_PORT}." >> &DEPLOY_LOG
 exit 0
